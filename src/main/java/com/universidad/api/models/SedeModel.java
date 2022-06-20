81e198d3c8dc1e2f0f324e0f2c9f83383172ad1f
Usuario1 <carlosj616@outlook.com>
2022-06-19 21:58:03 -0500

    Proyecto base con modelos

---
+++ src/main/java/com/universidad/api/models/SedeModel.java
@@ -0,0 +1,42 @@
+package com.universidad.api.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "sedes")
+public class SedeModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "estado")
+    private Boolean estado;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
